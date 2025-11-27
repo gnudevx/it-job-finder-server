3624c9c9dffbd38cd98827175d950e31c3b38f7d
Tin <nguyenductin011011@gmail.com>
2025-11-27 14:40:11 +0700

    ChangePassword for candidate

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
 
 export const getPersonalInfo = async (req, res) => {
   try {
@@ -37,6 +38,44 @@ export const updatePersonalInfo = async (req, res) => {
       success: true,
       message: 'Cập nhật thông tin user thành công',
       data: updatedUser,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: 'Server error',
+      error: err.message,
+    });
+  }
+};
+
+export const changePassword = async (req, res) => {
+  try {
+    const { currentPassword, newPassword } = req.body;
+
+    const user = await User.findById(req.user.id).select('+password');
+    if (!user) return res.status(404).json({ message: 'User not found' });
+
+    if (!user.passwordHash) {
+      return res
+        .status(400)
+        .json({ message: 'User không có passwordHash trong DB' });
+    }
+
+    // kiểm tra mật khẩu cũ
+    const isMatch = await bcrypt.compare(currentPassword, user.passwordHash);
+    if (!isMatch)
+      return res
+        .status(400)
+        .json({ message: 'Mật khẩu hiện tại không chính xác!' });
+
+    // hashing mật khẩu mới
+    const salt = await bcrypt.hash(newPassword, 10);
+    user.passwordHash = await bcrypt.hash(newPassword, salt);
+
+    await user.save();
+
+    return res.json({
+      success: true,
+      message: 'Đổi mật khẩu thành công!',
     });
   } catch (err) {
     return res.status(500).json({
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {
   getPersonalInfo,
   updatePersonalInfo,
+  changePassword,
 } from '../controllers/user.controller.js';
 import { verifyToken } from '../middlewares/jwt.js';
 
@@ -9,5 +10,6 @@ const router = express.Router();
 
 router.get('/personal-info', verifyToken, getPersonalInfo);
 router.put('/personal-info', verifyToken, updatePersonalInfo);
+router.put('/change-password', verifyToken, changePassword);
 
 export default router;
