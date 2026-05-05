d5b8b602a1b07264f4c40675aa13f822b9ed72c8
Nguyen Duc Dung <gnudevx@gmail.com>
2026-05-05 12:18:46 +0700

    "server conversation"

--- src/server.js
+++ src/server.js
@@ -51,7 +51,7 @@ io.on('connection', (socket) => {
     const updated = await Conversation.findByIdAndUpdate(
       conversationId,
       {
-        lastMessage: message.text,
+        lastMessage: message.type === 'file' ? '📎 File' : message.text || '',
         lastMessageTime: message.createdAt,
         $inc: {
           'unreadCount.employer': isEmployerSender ? 0 : 1,
