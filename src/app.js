f9d5c221a5e3d920d61bd3f5ac5b96cb54e70088
Nguyen Duc Dung <gnudevx@gmail.com>
2026-06-25 14:10:24 +0700

    "app build"

--- src/app.js
+++ src/app.js
@@ -70,7 +70,11 @@ const allowedOrigins = [
 app.use(
   cors({
     origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
+      if (
+        !origin ||
+        allowedOrigins.includes(origin) ||
+        /\.vercel\.app$/.test(origin) // thêm dòng này
+      ) {
         callback(null, true);
       } else {
         callback(new Error('Not allowed by CORS'));
