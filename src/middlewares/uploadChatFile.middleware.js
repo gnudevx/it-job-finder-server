b1143705e264df8cabb3b0230369d32c92e2a089
Nguyen Duc Dung <gnudevx@gmail.com>
2026-06-28 22:23:02 +0700

    "xóa gọi video + upload chat + mess hiển thị pdf"

--- src/middlewares/uploadChatFile.middleware.js
+++ src/middlewares/uploadChatFile.middleware.js
@@ -1,19 +1,8 @@
-// middlewares/uploadChatFile.js
 import { createUploader } from '../config/cloudinary.js';
 
 const uploadChatFile = createUploader({
   folder: 'chat',
-  allowedFormats: [
-    'pdf',
-    'png',
-    'jpg',
-    'jpeg',
-    'gif',
-    'webp',
-    'svg',
-    'doc',
-    'docx',
-  ],
+  allowedFormats: ['pdf', 'doc', 'docx', 'png', 'jpg', 'jpeg', 'gif', 'webp'],
   fileSizeMB: 10,
 });
 
--- src/routes/message.routes.js
+++ src/routes/message.routes.js
@@ -3,7 +3,6 @@ import { markAsRead } from '../controllers/message.controller.js';
 import Message from '../models/message.model.js';
 import uploadChatFile from '../middlewares/uploadChatFile.middleware.js';
 const router = express.Router();
-
 // đánh dấu đã đọc
 router.post('/mark-as-read', markAsRead);
 
@@ -28,6 +27,8 @@ router.post('/send-file', uploadChatFile.single('file'), async (req, res) => {
     const { conversationId, senderId } = req.body;
     const file = req.file;
 
+    console.log('URL:', file.path); // paste log này lên
+
     const message = await Message.create({
       conversationId,
       senderId,
@@ -40,12 +41,9 @@ router.post('/send-file', uploadChatFile.single('file'), async (req, res) => {
       },
     });
 
-    // emit realtime
-
     res.json(message);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
-
 export default router;
